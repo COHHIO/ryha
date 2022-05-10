e2cfb982faa5c0322e596b988cba1a820c3f26b1
mthomas-ketchbrook <mthomas@ketchbrookanalytics.com>
2022-05-09 23:16:35 -0400

    Developed `get_export_dates()` function, writing first set of unit tests for data quality

--- R/utils_read_data.R
+++ R/utils_read_data.R
@@ -5,9 +5,9 @@
 #' @return The return value, if any, from executing the utility.
 #'
 #' @noRd
-read_client <- function() {
+read_ethnicity <- function() {
 
-  readr::read_csv(
+  client <- readr::read_csv(
     file = here::here("data/Client.csv"),
     col_select = c(PersonalID, DOB, AmIndAKNative:GenderNone),
     col_types = readr::cols(
@@ -17,6 +17,36 @@ read_client <- function() {
     )
   ) |>
     dplyr::rename(HispanicLatino = Ethnicity)
+
+  ethnicity <- client |>
+    dplyr::select(-DOB) |>
+    dplyr::select(PersonalID, AmIndAKNative:HispanicLatino) |>
+    tidyr::pivot_longer(
+      cols = -PersonalID,
+      names_to = "Ethnicity",
+      values_to = "Status",
+      values_transform = list(Status = as.integer)
+    ) |>
+    dplyr::filter(Status == 1L) |>
+    dplyr::select(-Status)
+
+  gender <- client |>
+    dplyr::select(-DOB) |>
+    dplyr::select(PersonalID, Female:Questioning) |>
+    tidyr::pivot_longer(
+      cols = -PersonalID,
+      names_to = "Gender",
+      values_to = "Status",
+      values_transform = list(Status = as.integer)
+    ) |>
+    dplyr::filter(Status == 1L) |>
+    dplyr::select(-Status)
+
+  list(
+    client_full = client,
+    ethnicity = ethnicity,
+    gender = gender
+  )
 
 }
 
@@ -38,22 +68,123 @@ prep_heatmap <- function(data) {
 
 
 
-prep_bar_chart <- function(data) {
+generate_bar_chart <- function(data, group) {
 
   data |>
-    dplyr::select(-DOB) |>
-    dplyr::select(PersonalID, AmIndAKNative:HispanicLatino) |>
-    tidyr::pivot_longer(
-      cols = -PersonalID,
-      names_to = "Ethnicity",
-      values_to = "Status",
-      values_transform = list(Status = as.integer)
-    ) |>
-    dplyr::filter(Status == 1L) |>
     dplyr::count(
-      Ethnicity,
+      .data[[group]],
       name = "Count"
     ) |>
-    dplyr::arrange(Count)
+    dplyr::arrange(Count) |>
+    echarts4r::e_charts_(x = group) |>
+    echarts4r::e_bar(
+      serie = Count,
+      name = "# of Youth",
+      legend = FALSE,
+      label = list(
+        formatter = '{@[0]}',
+        show = TRUE,
+        position = "right"
+      )
+    ) |>
+    echarts4r::e_flip_coords()
+
+}
+
+
+
+# Master function for reading in all .csv data and converting to .parquet files
+get_export_dates <- function(dir) {
+
+  # Ensure there is a file in the directory titled "Export.csv"
+  export_file_exists <- fs::file_exists(
+    path = fs::path(dir, "Export.csv")
+  )
+
+  if (!export_file_exists) {
+
+    rlang::abort("Could not find `Export.csv` file in directory")
+
+  }
+
+  # Make sure that the "ExportStartDate" and "ExportEndDate" columns exist in
+  # the .csv
+  header <- readLines(
+    con = fs::path(dir, "Export.csv"),
+    n = 1
+  )
+
+  col_names_check <- c(
+    stringr::str_detect(
+      string = header,
+      pattern = "ExportStartDate",
+      negate = TRUE
+    ),
+    stringr::str_detect(
+      string = header,
+      pattern = "ExportEndDate",
+      negate = TRUE
+    )
+  )
+
+  if (any(col_names_check)) {
+
+    paste0(
+      "Could not find ",
+      ifelse(col_names_check[1], "`ExportStartDate` "),
+      ifelse(all(col_names_check), "and "),
+      ifelse(col_names_check[2], "`ExportEndDate` "),
+      "columns in the `Export.csv` file"
+    ) |>
+      rlang::abort()
+
+  }
+
+  # Get the date range of the export
+  export_data <- readr::read_csv(
+    file = fs::path(dir, "Export.csv"),
+    col_select = c(ExportStartDate, ExportEndDate),
+    col_types = readr::cols(
+      .default = readr::col_date()
+    )
+  )
+
+  # Ensure that neither of the two dates are NA values
+  check_nas <- c(
+    export_data$ExportStartDate[1],
+    export_data$ExportEndDate[1]
+  ) |>
+    is.na()
+
+  if (any(check_nas)) {
+
+    paste0(
+      "A valid ",
+      ifelse(check_nas[1], "`ExportStartDate` ", ""),
+      ifelse(all(check_nas), "and "),
+      ifelse(check_nas[2], "`ExportEndDate` ", ""),
+      "could not be found in `Export.csv`"
+    ) |>
+      rlang::abort()
+
+  }
+
+  num_rows <- nrow(export_data)
+
+  # Ensure that there was exactly one row
+  if (num_rows != 1L) {
+
+    paste0(
+      "Expected exactly 1 row of data, but found ",
+      num_rows, "rows."
+    ) |>
+      rlang::abort()
+
+  }
+
+  list(
+    export_start_date = export_data$ExportStartDate[1],
+    export_end_date = export_data$ExportEndDate[1]
+  )
 
 }
--- tests/testthat/test-utils_read_data.R
+++ tests/testthat/test-utils_read_data.R
@@ -1,3 +1,22 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("`get_export_dates()` throws error if nrow != 1", {
+
+  # Create a temp file
+  dir <- fs::path_temp()
+
+  # Write the headers to the temp file
+  writeLines(
+    text = "ExportStartDate, ExportEndDate",
+    con = fs::path(dir, "Export.csv")
+  )
+
+  headers_only <- export_file
+
+  # Ensure that the file containing only headers (no rows of data) throws an
+  # error
+  testthat::expect_error(
+    get_export_dates(dir = dirname(headers_only)),
+    "Expected exactly 1 row of data, but found 0 rows"
+  )
+
 })
