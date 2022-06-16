8b6f3719bea41982d8f73e5e3b46ce420d84b6dd
mthomas-ketchbrook <mthomas@ketchbrookanalytics.com>
2022-06-16 11:38:51 -0400

    Re-factoring `menuItems` functions into single script

--- R/fct_menuItems.R
+++ R/fct_menuItems.R
@@ -1,14 +1,52 @@
-menuItems <- tibble::tribble(
-  ~text ,            ~tabName                  , ~icon,
-  "Overview"         , "overview_page"         , "home",
-  "Gender"           , "gender_page"           , "venus-mars",
-  "Ethnicity"        , "ethnicity_page"        , "globe-americas",
-  "Veteran"          , "veteran_page"          , "fighter-jet",
-  "Disabilities"     , "disabilities_page"     , "accessible-icon",
-  "Employment"       , "employment_page"       , "briefcase",
-  "Education"        , "education_page"        , "book-open",
-  "Exits"            , "exits_page"            , "door-open",
-  "Health"           , "health_page"           , "stethoscope",
-  "Domestic Violence", "domestic_violence_page", "user-shield",
-  "Upload Data"      , "upload_page"           , "upload"
-)
+
+
+
+#' Define List of Menu Items
+#'
+#' @return A tibble, containing the menu title, id, and icon
+#'
+#' @details Note: Font-Awesome v5 is used for the icons
+#'
+#' @noRd
+define_menu <- function() {
+
+  tibble::tribble(
+    ~text              , ~tabName                , ~icon,
+    "Overview"         , "overview_page"         , "home",
+    "Gender"           , "gender_page"           , "venus-mars",
+    "Ethnicity"        , "ethnicity_page"        , "globe-americas",
+    "Veteran"          , "veteran_page"          , "fighter-jet",
+    "Disabilities"     , "disabilities_page"     , "accessible-icon",
+    "Employment"       , "employment_page"       , "briefcase",
+    "Education"        , "education_page"        , "book-open",
+    "Exits"            , "exits_page"            , "door-open",
+    "Health"           , "health_page"           , "stethoscope",
+    "Domestic Violence", "domestic_violence_page", "user-shield",
+    "Upload Data"      , "upload_page"           , "upload"
+  )
+
+}
+
+
+
+#' Create {bs4Dash} `menuItems` based on a dataset
+#'
+#' @details The input dataset to this function should be managed by the custom
+#'   `define_menu()` function
+#'
+#' @param menuItems A tibble with columns `text`, `tabName`, and `icon`; this
+#'   tibble should be the result of calling `define_menu()`
+#'
+#' @noRd
+create_menuItems <- function(menuItems){
+  purrr::pmap(
+    .l = list(menuItems$text, menuItems$tabName, menuItems$icon),
+    .f = function(text, tabName, icon) {
+      bs4Dash::menuItem(
+        text = text,
+        tabName = tabName,
+        icon = shiny::icon(icon)
+      )
+    }
+  )
+}
