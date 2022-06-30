16c951df6ee45d6e639eae3fe0dd9f5ff48ef1d3
Ivan Millanes <ivanmillanes26@gmail.com>
2022-06-30 19:23:31 -0300

    Add filter module server
    The idea here is to take the static dm object created when we launched the app and create a reactive filtered dm which will be used in charts

--- R/mod_filters.R
+++ R/mod_filters.R
@@ -77,9 +77,20 @@ mod_filters_ui <- function(id){
 #' filters Server Functions
 #'
 #' @noRd
-mod_filters_server <- function(id){
+mod_filters_server <- function(id, dm){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+
+    my_dm_filtered <- shiny::reactive({
+      dm |>
+        dm::dm_filter(table_client,
+                      gender %in% input$gender,
+                      ethnicity %in% input$ethnicity,
+                      veteran_status %in% input$veteran_status) |>
+        dm::dm_apply_filters()
+    })
+
+    return(my_dm_filtered)
 
   })
 }
