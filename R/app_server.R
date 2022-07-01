5674eb00a04f56d9015c7ed828ded4c72bf913ca
Ivan Millanes <ivanmillanes26@gmail.com>
2022-07-01 18:51:38 -0300

    Modify dm object innitialization
    In order to create the dm object in the server, I wrap its creation in a function that gets called in the server.

--- R/app_server.R
+++ R/app_server.R
@@ -6,6 +6,9 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
+  my_dm <- create_dm()
+
   my_dm_filtered <- mod_filters_server("filters_1", my_dm)
+
   mod_client_server("client_1", my_dm_filtered)
 }
--- R/fct_create_dm.R
+++ R/fct_create_dm.R
@@ -12,77 +12,81 @@
 # We might end up refactoring this code. I wanted to make things work as soon
 #  as possible.
 
-# Connect to DB
-con <- DBI::dbConnect(
-  drv = RPostgres::Postgres(),
-  dbname = Sys.getenv("POSTGRES_DBNAME"),
-  host = Sys.getenv("POSTGRES_HOST"),
-  port = Sys.getenv("POSTGRES_PORT"),
-  user = Sys.getenv("POSTGRES_USER"),
-  password = Sys.getenv("POSTGRES_PWD")
-)
-
-# Data wrangling
-#  Read the tables into memory and process them. This might not be ideal, but
-#  table size is small for now.
-table_client <- DBI::dbReadTable(conn = con, name = "client") |>
-  dplyr::mutate(
-
-    # Create ethnicity column
-    ethnicity = dplyr::case_when(
-      am_ind_ak_native == "Yes" ~ "American Indian and Alaska Native",
-      asian == "Yes" ~ "Asian",
-      black_af_american == "Yes" ~ "Black and African American",
-      native_hi_pacific == "Yes" ~ "Native Hawaiians and other Pacific Islanders",
-      white == "Yes" ~ "White",
-      !is.na(race_none) ~ "Missing Data",
-      hispanic_latinaox == "Yes" ~ "Hispanic and Latino American",
-      TRUE ~ "Missing Data"
-    ),
-
-    # Create gender column
-    gender = dplyr::case_when(
-      transgender == "Yes" ~ "Transgender",
-      questioning == "Yes" ~ "Questioning",
-      no_single_gender == "Yes" ~ "No Single Gender",
-      female == "Yes" ~ "Female",
-      male == "Yes" ~ "Male",
-      TRUE ~ "Missing Data"
-    ),
-
-    # Create veteran status column
-    veteran_status = dplyr::case_when(
-      veteran_status == "Yes" ~ "Yes",
-      veteran_status == "No" ~ "No",
-      TRUE ~ "Missing Data"
-    ),
-
-    # Create age column
-    age = lubridate::interval(dob, lubridate::today()) / lubridate::years(1)
+create_dm <- function(){
+  # Connect to DB
+  con <- DBI::dbConnect(
+    drv = RPostgres::Postgres(),
+    dbname = Sys.getenv("POSTGRES_DBNAME"),
+    host = Sys.getenv("POSTGRES_HOST"),
+    port = Sys.getenv("POSTGRES_PORT"),
+    user = Sys.getenv("POSTGRES_USER"),
+    password = Sys.getenv("POSTGRES_PWD")
   )
 
-table_submission <- DBI::dbReadTable(conn = con, name = "submission") |>
-  dplyr::mutate(
-    # Create submission quarter column
-    quarter = paste0(
-      lubridate::year(export_start_date),
-      " Q",
-      lubridate::quarter(export_start_date)
+  # Data wrangling
+  #  Read the tables into memory and process them. This might not be ideal, but
+  #  table size is small for now.
+  table_client <- DBI::dbReadTable(conn = con, name = "client") |>
+    dplyr::mutate(
+
+      # Create ethnicity column
+      ethnicity = dplyr::case_when(
+        am_ind_ak_native == "Yes" ~ "American Indian and Alaska Native",
+        asian == "Yes" ~ "Asian",
+        black_af_american == "Yes" ~ "Black and African American",
+        native_hi_pacific == "Yes" ~ "Native Hawaiians and other Pacific Islanders",
+        white == "Yes" ~ "White",
+        !is.na(race_none) ~ "Missing Data",
+        hispanic_latinaox == "Yes" ~ "Hispanic and Latino American",
+        TRUE ~ "Missing Data"
+      ),
+
+      # Create gender column
+      gender = dplyr::case_when(
+        transgender == "Yes" ~ "Transgender",
+        questioning == "Yes" ~ "Questioning",
+        no_single_gender == "Yes" ~ "No Single Gender",
+        female == "Yes" ~ "Female",
+        male == "Yes" ~ "Male",
+        TRUE ~ "Missing Data"
+      ),
+
+      # Create veteran status column
+      veteran_status = dplyr::case_when(
+        veteran_status == "Yes" ~ "Yes",
+        veteran_status == "No" ~ "No",
+        TRUE ~ "Missing Data"
+      ),
+
+      # Create age column
+      age = lubridate::interval(dob, lubridate::today()) / lubridate::years(1)
     )
-  )
 
-table_project <- DBI::dbReadTable(conn = con, name = "project")
-table_current_living_situation <- DBI::dbReadTable(conn = con, name = "current_living_situation")
+  table_submission <- DBI::dbReadTable(conn = con, name = "submission") |>
+    dplyr::mutate(
+      # Create submission quarter column
+      quarter = paste0(
+        lubridate::year(export_start_date),
+        " Q",
+        lubridate::quarter(export_start_date)
+      )
+    )
 
-DBI::dbDisconnect(conn = con)
+  table_project <- DBI::dbReadTable(conn = con, name = "project")
+  table_current_living_situation <- DBI::dbReadTable(conn = con, name = "current_living_situation")
 
-# Create dm object, defining keys
-my_dm <- dm::dm(table_client,
-                table_submission,
-                table_project,
-                table_current_living_situation) |>
-  dm::dm_add_pk(table = table_submission, columns = submission_id) |>
-  dm::dm_add_pk(table = table_client, columns = personal_id) |>
-  dm::dm_add_fk(table = table_client, columns = submission_id, ref_table = table_submission) |>
-  dm::dm_add_fk(table = table_current_living_situation, columns = personal_id, ref_table = table_client)
+  DBI::dbDisconnect(conn = con)
 
+  # Create dm object, defining keys
+  my_dm <- dm::dm(table_client,
+                  table_submission,
+                  table_project,
+                  table_current_living_situation) |>
+    dm::dm_add_pk(table = table_submission, columns = submission_id) |>
+    dm::dm_add_pk(table = table_client, columns = personal_id) |>
+    dm::dm_add_fk(table = table_client, columns = submission_id, ref_table = table_submission) |>
+    dm::dm_add_fk(table = table_current_living_situation, columns = personal_id, ref_table = table_client)
+
+  return(my_dm)
+
+}
