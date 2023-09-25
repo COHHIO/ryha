4409179ca7c3a1c63b987764b9b2d99167284903
Ivan Millanes <ivanmillanes26@gmail.com>
2023-09-25 10:46:34 -0300

    Add server logic to apply filter

--- R/mod_filters.R
+++ R/mod_filters.R
@@ -237,11 +237,23 @@ mod_filters_server <- function(id, dm, w, rctv){
 
       }
 
+      # Filter head of household using enrollment data
+      if (input$heads_of_household_global == TRUE) {
+
+        enrollment <- dm$enrollment |>
+          dplyr::filter(relationship_to_ho_h == "Self (head of household)")
+
+      } else {
+
+        enrollment <- dm$enrollment
+
+      }
+
       out <- dm$project |>
         dplyr::filter(project_name %in% input$project_filter_global) |>
         dplyr::select(project_id) |>
         dplyr::inner_join(
-          dm$enrollment |>
+          enrollment |>
             # Remove individuals who entered *after* the later active date
             dplyr::filter(
               entry_date <= input$active_date_filter_global[2]
