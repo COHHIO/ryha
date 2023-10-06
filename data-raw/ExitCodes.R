9f6a95315e5f7fe31a172652aba9200ef0657c61
Ivan Millanes <ivanmillanes26@gmail.com>
2023-10-06 16:11:32 -0300

    Update ProjectCompletionStatus codes

--- data-raw/ExitCodes.R
+++ data-raw/ExitCodes.R
@@ -3,8 +3,8 @@
 ProjectCompletionStatusCodes <- tibble::tribble(
   ~Code, ~Description,
   1L, "Completed project",
-  2L, "Youth voluntarily left early",
-  3L, "Youth was expelled or otherwise involuntarily discharged from project"
+  2L, "Client voluntarily left early",
+  3L, "Client was expelled or otherwise involuntarily discharged from project"
 )
 
 CountExchangeForSexCodes <- tibble::tribble(
