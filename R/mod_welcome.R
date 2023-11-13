ded1ee502657f6dca555e8dce93fbb0ede370170
Ivan Millanes <ivanmillanes26@gmail.com>
2023-11-13 13:25:27 -0300

    Add vertical centered logos

---
+++ R/mod_welcome.R
@@ -0,0 +1,55 @@
+#' welcome UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+mod_welcome_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+
+    # Add vertical-centered logos
+    shiny::fluidRow(
+
+        shiny::column(
+          width = 3,
+          shiny::img(src = "www/odh_logo.png", width = "100%")
+        ) |>
+          shiny::tagAppendAttributes(class = "vertical-center"),
+
+        shiny::column(
+            width = 6,
+            shiny::img(src = "www/cohhio_logo.png", width = "100%")
+        ) |>
+          shiny::tagAppendAttributes(class = "vertical-center"),
+
+        shiny::column(
+          width = 3,
+          shiny::img(src = "www/ketchbrook_logo.png", width = "100%")
+        ) |>
+          shiny::tagAppendAttributes(class = "vertical-center")
+
+      )
+
+    )
+
+}
+
+#' welcome Server Functions
+#'
+#' @noRd
+mod_welcome_server <- function(id){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+
+  })
+}
+
+## To be copied in the UI
+# mod_welcome_ui("welcome_1")
+
+## To be copied in the server
+# mod_welcome_server("welcome_1")
