50ee3b2ab3c334ca2ce7734675598ddcc1ded6ff
Michael Thomas <mthomas@ketchbrookanalytics.com>
2023-12-05 00:26:47 -0500

    Using `glue::glue_sql()` for safety & conciseness
    and minor roxygen tweaks

--- R/fct_create_dm.R
+++ R/fct_create_dm.R
@@ -395,34 +395,38 @@ client_tbl <- read_data_from_table(
 
 }
 
-#' Read data from table
+#' Read specific columns from database table
 #'
-#' \code{read_data_from_table()} reads specific data from a SQL table based on
-#' provided column names.
+#' \code{read_data_from_table()} provides an expedited way to query a select,
+#' user-specified set of columns from a SQL database table.
 #'
-#' @param connection A DBI database connection object.
-#' @param table_name The name of the SQL table from which to read the data.
+#' @param connection A DBI database connection object
+#' @param table_name The name of the SQL table from which to read the data
 #' @param column_names A character vector specifying the column names to read
-#' from the table.
+#' from the table
 #'
 #' @return A data frame containing the requested data from the specified columns
-#' in the table.
+#' in the table
 #'
 #' @examples
 #' # Establish connection to PostgreSQL database
 #' con <- connect_to_db()
+#'
+#' # Query only the "project_name" and "project_id" columns from the 'project'
+#' # database table
 #' read_data_from_table(
 #'   connection = con,
 #'   table_name = "project",
 #'   column_names = c("project_name", "project_id")
 #' )
 read_data_from_table <- function(connection, table_name, column_names) {
-  column_names_string <- paste0(column_names, collapse = ", ")
 
   DBI::dbGetQuery(
     conn = connection,
-    statement = glue::glue(
-      "SELECT {column_names_string} FROM {table_name}"
+    statement = glue::glue_sql(
+      "SELECT {`column_names`*} FROM {`table_name`}",
+      .con = con
     )
   )
+
 }
