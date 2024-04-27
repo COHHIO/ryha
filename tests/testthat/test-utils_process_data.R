fe0359afe67970cef8dc4c9bcaa801f23d6e7855
Ivan Millanes <ivanmillanes26@gmail.com>
2024-04-27 17:35:28 -0300

    Add unit test for find_file() function

--- tests/testthat/test-utils_process_data.R
+++ tests/testthat/test-utils_process_data.R
@@ -1,3 +1,10 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("find_file returns correct filepath", {
+
+  mock_filepaths <- c(
+    "some/path/to/file/data1.csv",
+    "some/path/to/file/data2.csv"
+  )
+
+  expect_equal(find_file(mock_filepaths, "data1"), "some/path/to/file/data1.csv")
+
 })
