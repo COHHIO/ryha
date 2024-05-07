789356a90bd92ba1f05e2accca37d73fe74519a9
Ivan Millanes <ivanmillanes26@gmail.com>
2024-05-07 11:25:39 -0300

    Update find_file test

--- tests/testthat/test-utils_process_data.R
+++ tests/testthat/test-utils_process_data.R
@@ -1,13 +1,22 @@
 test_that("find_file returns correct filepath", {
 
-  mock_filepaths <- c(
-    "some/path/to/file/data1.csv",
-    "some/path/to/file/data2.csv"
+  temp_dir <- tempdir()
+
+  write.csv(
+    data.frame(x = 1:5, y = letters[1:5]),
+    file = file.path(temp_dir, "data1.csv"),
+    row.names = FALSE
+  )
+
+  write.csv(
+    data.frame(x = 1:5, y = letters[1:5]),
+    file = file.path(temp_dir, "data2.csv"),
+    row.names = FALSE
   )
 
   expect_equal(
-    find_file(mock_filepaths, "data1"),
-    "some/path/to/file/data1.csv"
+    find_file(fs::dir_ls(temp_dir), "data1"),
+    file.path(normalizePath(temp_dir, winslash = "/"), "data1.csv")
   )
 
 })
