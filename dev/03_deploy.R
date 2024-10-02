88baa79e8d748ed0a473eed459f650d5999a0f37
Ivan Millanes <ivanmillanes26@gmail.com>
2024-10-02 09:32:53 -0300

    Remove unnecessary lines

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -27,19 +27,8 @@ devtools::build()
 
 ## RStudio ----
 ## If you want to deploy on RStudio related platforms
-golem::add_rstudioconnect_file()
 golem::add_shinyappsio_file()
-golem::add_shinyserver_file()
 
-## Docker ----
-## If you want to deploy via a generic Dockerfile
-golem::add_dockerfile()
-
-## If you want to deploy to ShinyProxy
-golem::add_dockerfile_shinyproxy()
-
-## If you want to deploy to Heroku
-golem::add_dockerfile_heroku()
 ## Deploy to Posit Connect or ShinyApps.io ----
 
 ## In command line:
